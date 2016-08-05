7708ebcb35d02862c9298c178a6dcf30076c4875
齋藤陸弥 <Rikuya@Rikuya-mac.stama1.kt.home.ne.jp>
2016-08-05 23:36:15 +0900

    Chapter5 終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'top#index'
-  resources :top, except: [:new, :show, :create, :edit]
   get 'about' => 'top#about', as: 'about'
+  resources :top, only: [:index]
+  resources :members do
+    collection { get 'search' }
+  end
 end
