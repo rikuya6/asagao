f14e89b544470b17beb92142ef40694979d1970f
齋藤陸弥 <rikuya6@gmail.com>
2016-08-07 21:32:33 +0900

    Chapter7.3終了 p313

--- test/factories/members.rb
+++ test/factories/members.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: members
+#
+#  id            :integer          not null, primary key
+#  number        :integer          not null
+#  name          :string           not null
+#  full_name     :string
+#  email         :string
+#  birthday      :date
+#  gender        :integer          default(0), not null
+#  administrator :boolean          default(FALSE), not null
+#  created_at    :datetime
+#  updated_at    :datetime
+#
+
 FactoryGirl.define do
   factory :member do
     sequence(:number) { |n| n + 1 }
--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: members
+#
+#  id            :integer          not null, primary key
+#  number        :integer          not null
+#  name          :string           not null
+#  full_name     :string
+#  email         :string
+#  birthday      :date
+#  gender        :integer          default(0), not null
+#  administrator :boolean          default(FALSE), not null
+#  created_at    :datetime
+#  updated_at    :datetime
+#
+
 require 'test_helper'
 
 class MemberTest < ActiveSupport::TestCase
