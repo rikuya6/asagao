ec44a0e8ad8e6c1ce6803c192c0855e75fa0e6e5
齋藤陸弥 <rikuya6@gmail.com>
2016-08-12 17:04:27 +0900

    Chapter7 p325終了 p326からスタート

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,18 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.order(released_at: :desc)
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+end
