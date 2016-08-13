eea1330f300bcdff1a1735409239801881754da4
齋藤陸弥 <rikuya6@gmail.com>
2016-08-14 00:12:42 +0900

    p359終了

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160813000000) do
+ActiveRecord::Schema.define(version: 20160813145513) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",                       null: false
