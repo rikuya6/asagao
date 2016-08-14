93881795bef67320c741159c8e1b49c723abd740
齋藤陸弥 <rikuya6@gmail.com>
2016-08-14 14:03:00 +0900

    Chapter8.2終了 p376スタート

--- db/seeds/development/members.rb
+++ db/seeds/development/members.rb
@@ -1,7 +1,18 @@
 names = %w(Taro Jiro Hana John Mike Sophy Bill Alex Mary Tom)
 fnames = ["佐藤", "鈴木", "高橋", "田中"]
 gnames = ["太郎", "次郎", "花子"]
-0.upto(9) do |idx|
+Member.create(
+  number: 10,
+  name: 'Rikuya',
+  full_name: 'Rikuya Saito',
+  email: 'rikuya@test.com',
+  birthday: Date.current,
+  gender: 0,
+  administrator: true,
+  password: 'password',
+  password_confirmation: 'password'
+)
+1.upto(10) do |idx|
   Member.create(
     number: idx + 10,
     name: names[idx],
