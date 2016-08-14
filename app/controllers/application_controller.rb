0885b1f724421296602470a68e2e25314cfdefe0
齋藤陸弥 <rikuya6@gmail.com>
2016-08-14 22:38:11 +0900

    p394終了本番テスト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,20 @@ class ApplicationController < ActionController::Base
 
   class Forbidden < StandardError; end
 
+  class NotFound < StandardError; end
+
+  if Rails.env.production?
+    rescue_from Exception,                          with: :rescue_500
+    rescue_from ActionController::RoutingError,     with: :rescue_404
+    rescue_from ActiveRecord::RecordNotFound,       with: :rescue_404
+    rescue_from ActionController::ParameterMissing, with: :rescue_400
+  end
+
+  rescue_from Forbidden, with: :rescue_403
+  rescue_from NotFound,  with: :rescue_404
+
   private
+
   def current_member
     Member.find_by(id: session[:member_id]) if session[:member_id]
   end
@@ -13,5 +26,21 @@ class ApplicationController < ActionController::Base
 
   def login_required
     raise Forbidden unless current_member
+  end
+
+  def rescue_400(exception)
+    render 'errors/bad_request', status: 400, layout: 'error', formats: [:html]
+  end
+
+  def rescue_403(exception)
+    render 'errors/forbidden',   status: 403, layout: 'error', formats: [:html]
+  end
+
+  def rescue_404(exception)
+    render 'errors/not_found',   status: 404, layout: 'error', formats: [:html]
+  end
+
+  def rescue_500(exception)
+    render 'errors/internal_server_error', status: 500, layout: 'error', formats: [:html]
   end
 end
